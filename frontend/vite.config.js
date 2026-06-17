d66a2820fa42bd10ff69d20e78da0de80d8bfcd5
Mathraaj <mathraaj222@gmail.com>
2026-06-18 01:13:07 +0800

    Frontend API URL Fix1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const VITE_API_URL = import.meta.env.VITE_API_URL
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      '/api/': {
+        target: VITE_API_URL || 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       }
