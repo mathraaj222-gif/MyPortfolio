a2f32090b49739eabb07adc1502de84b72f58ca8
Mathraaj <mathraaj222@gmail.com>
2026-09-22 19:11:47 +0800

    Deploying Frontend Admin on Amplify

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Admin dashboard Vite config
+// Admin dashboard Vite config 2
 export default defineConfig({
   plugins: [react()],
   server: {
